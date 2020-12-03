39bc482b16a083ccb2d4632ebff6d7c1d56ea2c1
poldinik <lorenzo.vannucchi1993@gmail.com>
2020-12-03 18:14:40 +0100

    prova a creare user

--- create_testuser.sh
+++ create_testuser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -H "Content-Type: application/json" --data '{"username":"user1","password":"123.com", "email":"user1@qq.com"}' http://127.0.0.1:5000/api/v1/auth/register
+curl -H "Content-Type: application/json" --data '{"username":"user1","password":"123.com", "email":"user1@qq.com"}' http://localhost:8888/api/v1/auth/register
 curl -H "Content-Type: application/json" --data '{"username":"user2","password":"123.com", "email":"user2@qq.com"}' http://127.0.0.1:5000/api/v1/auth/register
 curl -H "Content-Type: application/json" --data '{"username":"user3","password":"123.com", "email":"user3@qq.com"}' http://127.0.0.1:5000/api/v1/auth/register
 curl -H "Content-Type: application/json" --data '{"username":"user4","password":"123.com", "email":"user4@qq.com"}' http://127.0.0.1:5000/api/v1/auth/register
