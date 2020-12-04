9e59917d9df117a57549752477203ebf3265f960
poldinik <lorenzo.vannucchi1993@gmail.com>
2020-12-04 14:28:12 +0100

    mette schema tool detail

--- gandalf_app/api/project/serializers.py
+++ gandalf_app/api/project/serializers.py
@@ -12,14 +12,56 @@ project_created_response = api.model('ProjectStatus', {
 })
 
 
-# project_recepit_response = api.model('ProjectReceipt', {
-#     'id': fields.Integer(required=True, description='Project Id'),
-#     'location': fields.String(required=True, description='Project location'),
-#     'name': fields.String(required=True, description='Project name'),
-# })
-#
-# project_details_response = api.model('ProjectDetails', {
-#     'id': fields.Integer(required=True, description='Project Id'),
-#     'location': fields.String(required=True, description='Project location'),
-#     'name': fields.String(required=True, description='Project name'),
-# })
+project_recepit_response = api.model('ProjectReceipt', {
+    'id': fields.Integer(required=True, description='Project Id'),
+    'location': fields.String(required=True, description='Project location'),
+    'name': fields.String(required=True, description='Project name'),
+})
+
+project_details_response = api.model('ProjectDetails', {
+    'id': fields.Integer(required=True, description='Project Id'),
+    'location': fields.String(required=True, description='Project location'),
+    'name': fields.String(required=True, description='Project name'),
+})
+
+media_receipt_response = api.model('MediaReceipt', {
+    'id': fields.Integer(required=True, description='Project Id'),
+    'location': fields.String(required=True, description='Project location'),
+    'name': fields.String(required=True, description='Project name'),
+})
+
+data_receipt_response = api.model('DataReceipt', {
+    'id': fields.Integer(required=True, description='Project Id'),
+    'location': fields.String(required=True, description='Project location'),
+    'name': fields.String(required=True, description='Project name'),
+})
+
+uploaded_file_response = api.model('UploadedFile', {
+    'id': fields.Integer(required=True, description='Project Id'),
+    'location': fields.String(required=True, description='Project location'),
+    'name': fields.String(required=True, description='Project name'),
+})
+
+uploaded_media_file_response = api.model('UploadedMediaFile', {
+    'id': fields.Integer(required=True, description='Project Id'),
+    'location': fields.String(required=True, description='Project location'),
+    'name': fields.String(required=True, description='Project name'),
+})
+
+uploaded_data_file_response = api.model('UploadedDataFile', {
+    'id': fields.Integer(required=True, description='Project Id'),
+    'location': fields.String(required=True, description='Project location'),
+    'name': fields.String(required=True, description='Project name'),
+})
+
+result_summary_response = api.model('ResultSummary', {
+    'id': fields.Integer(required=True, description='Project Id'),
+    'location': fields.String(required=True, description='Project location'),
+    'name': fields.String(required=True, description='Project name'),
+})
+
+result_details_response = api.model('ResultDetails', {
+    'id': fields.Integer(required=True, description='Project Id'),
+    'location': fields.String(required=True, description='Project location'),
+    'name': fields.String(required=True, description='Project name'),
+})
--- gandalf_app/api/tool/serializers.py
+++ gandalf_app/api/tool/serializers.py
@@ -1,3 +1,8 @@
 from flask_restplus import fields
 from gandalf_app.api.restplus import api
 
+tool_datail_response = api.model('ToolDetail', {
+        'email': fields.String(required=True, description='user email address'),
+        'old_password': fields.String(required=True, description='user old password'),
+        'new_password': fields.String(required=True, description='user new password'),
+})
