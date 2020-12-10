c8857e214c1bb28ab5a5830ad5bfba371e37f95e
poldinik <lorenzo.vannucchi1993@gmail.com>
2020-12-10 18:23:36 +0100

    splita codice test registrazione e login in due metodi

--- tests/test_gandalf.py
+++ tests/test_gandalf.py
@@ -7,16 +7,33 @@ logging.config.fileConfig(logging_conf_path)
 log = logging.getLogger(__name__)
 authToken = ''
 
+
 # ogni test method deve iniziare con test_
 
 # metodi da richiamare per ogni test
-def login():
-    pass
+def login(client):
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+    }
+    data = {
+        'username': "utente1",
+        'password': "pass1",
+        'email': "utente1@gmail.com"
+    }
 
-def register():
-    pass
+    loginUrl = '/api/v1/auth/login'
 
-def test_register_login(client):
+    loginResponse = client.post(loginUrl, data=json.dumps(data), headers=headers)
+    login_json_response = json.loads(loginResponse.get_data())
+    log.info(str(login_json_response))
+    authToken = login_json_response['access_token']
+    log.info("Setta token di accesso")
+    log.info(authToken)
+    return loginResponse
+
+
+def register(client):
     mimetype = 'application/json'
     headers = {
         'Content-Type': mimetype,
@@ -28,25 +45,23 @@ def test_register_login(client):
     }
 
     registerUrl = '/api/v1/auth/register'
-    loginUrl = '/api/v1/auth/login'
 
     registerResponse = client.post(registerUrl, data=json.dumps(data), headers=headers)
     register_json_response = json.loads(registerResponse.get_data())
     log.info(str(register_json_response))
+    return registerResponse
 
-    loginResponse = client.post(loginUrl, data=json.dumps(data), headers=headers)
-    login_json_response = json.loads(loginResponse.get_data())
-    log.info(str(login_json_response))
-    authToken = login_json_response['access_token']
-    log.info("Setta token di accesso")
-    log.info(authToken)
 
+def test_register_login(client):
+    registerResponse = register(client)
+    loginResponse = login(client)
     assert registerResponse.status_code == 200 and loginResponse.status_code == 200
 
 
 # ValueError: flask-restplus blueprints can only be registered once.
 # questo perchè la configurazione della app tramite conftest inizializza la app più volte? Andrebe fatto una volta sola
-# soluzione è mettere app app scoped in app.py e non chiamare ogni volta initialize sennà fa binding ogni volta del blueprint
+# soluzione è mettere app app scoped in app.py e non chiamare ogni volta initialize sennò fa binding ogni volta del blueprint
+# e blueprint può ovviamente essere attaccato una volta sola
 def test_example(client):
     response = client.get('/')
     log.info("token è " + str(authToken))
